70bee75f4209aa8c1cd20116530497302298e874
root <root@drnutsu-K46CM.(none)>
2014-05-26 22:43:44 +0700

    home

---
+++ app/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>HomeManagement</title>
+	<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />	
+</head>
+<body>
+	<div class = "well" >
+		test 
+	</div>
+</body>
+</html>
